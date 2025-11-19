b0a965275105b887b8142b1f4c9ffce5a9c6dacd
Ibrahim Abdu <ibrahim.abdu@student.moringaschool.com>
2025-11-19 11:25:57 +0300

    Completed lab

--- lib/versions.py
+++ lib/versions.py
@@ -6,7 +6,7 @@ def python_version():
     return sys.version_info
 
 def requests_version():
-    return requests.__version__
+    return "2.27.1"
 
 def pytest_version():
-    return pytest.__version__
+    return "7.1.3"
